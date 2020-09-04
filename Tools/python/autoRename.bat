4d7fb298d71d73feb7d9ca001d243ddfb3683f99
jesenzhang <jesen.z@foxmail.com>
2020-09-04 13:36:20 +0800

    windows 拖拽执行名称检查

---
+++ Tools/python/autoRename.bat
@@ -0,0 +1,19 @@
+@echo off
+
+REM set path=%~d0%~p0
+
+set PWD=%~dp0
+echo %PWD%
+
+python "%PWD%texture_checkname.py " --path  %1
+pause
+
+REM :start
+REM D:/App/Python/python.exe "%path%texture_checkname.py " --path  %1
+
+REM shift
+REM if NOT x%1==x goto start
+
+pause
+
+ 
